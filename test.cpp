e02a8c026545418e32a3d7662079b9fe76236419
ooo <121344064+victorsemipalatin@users.noreply.github.com>
2023-10-14 21:24:59 +0300

    написан класс для тестирования

--- test.cpp
+++ test.cpp
@@ -3,35 +3,53 @@
 #include "cache.hpp"
 
 
-long long int cache_func(long long int *test_case, int l) {
-    long long int size_of_cache;
-    long long int value, count = 0;
-    cache_t Cache_table;
-    size_of_cache = test_case[0];
-    Cache_table.size_of_cache = size_of_cache;
-    for (int i = 2; i < l; i++) {
-        value = test_case[i];
-        count += Cache_table.find_value(value);
-    }
-    return count;
+class Tcase{
+  int size_of_cache;
+  vector <int> cache_t;
+  int res_of_test;
+public:
+  Tcase(int, vector <int>, int);
+
+  int get_sizeof_cache() {return size_of_cache;}
+  vector <int> get_cache_t() {return cache_t;}
+  int get_res_of_test() {return res_of_test;}
+};
+
+
+Tcase::Tcase(int size_of_cache_, vector <int> cache_t_, int res_of_test_){
+  size_of_cache = size_of_cache_;
+  cache_t = cache_t_;
+  res_of_test = res_of_test_;
+}
+
+
+int test_func(Tcase test){
+  Cache <long long int, long long int> cache_table;
+  cache_table.size_of_cache = test.get_sizeof_cache();
+  auto value = test.get_cache_t();
+  int count = 0;
+  int l = value.size();
+  for(int i = 0; i < l; i++){
+    count += cache_table.find_value(value[i]);
+  }
+  return count;
 }
 
 
 int main(void){
-    long long int test_case_1[7] = {2, 5, 1, 2, 3, 2, 1};
-    long long int test_case_2[8] = {3, 6, 1, 1, 1, 1, 1, 1};
-    long long int test_case_3[7] = {2, 5, 1, 3, 2, 4, 2};
-    long long int test_case_4[8] = {4, 6, 1, 3, 4, 5, 3, 5};
-    long long int test_case_5[10] = {5, 8, 1, 2, 3, 4, 5, 5, 3, 1};
+  
+    vector <Tcase> test_cases = {Tcase(2, {1, 2, 3, 2, 1}, 1),
+                                 Tcase(3, {1, 1, 1, 1, 1, 1}, 5),
+                                 Tcase(2, {1, 3, 2, 4, 2}, 1),
+                                 Tcase(4, {1, 3, 4, 5, 3, 5}, 2),
+                                 Tcase(5, {1, 2, 3, 4, 5, 5, 3, 1}, 3)};
 
-    assert(cache_func(test_case_1, 7) == 1);
-    assert(cache_func(test_case_2, 8) == 5);
-    assert(cache_func(test_case_3, 7) == 1);
-    assert(cache_func(test_case_4, 8) == 2);
-    assert(cache_func(test_case_5, 10) == 3);
+    int l = test_cases.size();
+    for(int i = 0; i < l; i++){
+      assert(test_func(test_cases[i]) == test_cases[i].get_res_of_test());
+    } 
 
     std::cout << "All tests passed" << std::endl;
 
     return 0;    
 }
-
